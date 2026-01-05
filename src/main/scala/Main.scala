c2f645e0321ce846ba3bfae80b48cb2108b99b68
AlfonsoR1 <105025740+AlfonsoR1@users.noreply.github.com>
2026-01-05 17:33:57 -0600

    initial commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,14 @@
+
+
+object Main:
+  @main def run(): Unit =
+
+    //defining a value
+    val meaningOfLife: Int = 44 // means -> const int meaningOfLife = 42;
+
+    // STATEMENT BELLOW WILL ERROR because value is immutable
+    // val meaningOfLife = 44
+
+    val aBoolean =  false  // defining boolean type is optional as the compiler can define it
+
+    // Int, Boolean, Char, Double, Float, String are all Standard type
