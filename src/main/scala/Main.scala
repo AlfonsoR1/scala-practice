96c087e0d3aeab3705a71fe4691b7905cff657e7
AlfonsoR1 <105025740+AlfonsoR1@users.noreply.github.com>
2026-01-05 18:23:07 -0600

    added String operation examples and a if expression

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,4 +11,16 @@ object Main:
 
     val aBoolean =  false  // defining boolean type is optional as the compiler can define it
 
-    // Int, Boolean, Char, Double, Float, String are all Standard type
+    // Int, Boolean, Char, Double, Float, String are all Standard data type  structures
+
+    // String and String operations
+    val aString = "I love Scala"
+    val aComposedString = "I" + " love " + "Scala"
+    val anInterpolatedString = s"The meaning of life is $meaningOfLife"
+
+    // Expressions = structures that can be reduced to a value
+    val anExpression = 2+3
+
+    // if expression in other languages: meaningOfLife > 43 ? 56 : 999
+    val ifExpression = if (meaningOfLife > 43) 56 else 999
+
