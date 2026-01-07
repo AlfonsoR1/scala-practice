08530355bede7049217b5bd1908e9c17c2783445
AlfonsoR1 <105025740+AlfonsoR1@users.noreply.github.com>
2026-01-06 20:00:49 -0600

    Started Scala OOP examples

--- src/main/scala/OOP_Practice.scala
+++ src/main/scala/OOP_Practice.scala
@@ -1,3 +1,21 @@
+
+
 class OOP_Practice {
+  // CLASS AND INSTANCE
+  class Animal {
+    // define fields
+    val age = 0
+    //define methods
+    def eat() = println("I'm eating")
+  }
+  val anAnimal = new Animal
+
+  // INHERITANCE
+  class Dog(name: String) extends  Animal // contractor definition
+
+  val aDog = new Dog("Dolly")
+
+  // constructor arguments are NOT fields
+  // aDog.name  <- NOT ALLOWED
 
 }
